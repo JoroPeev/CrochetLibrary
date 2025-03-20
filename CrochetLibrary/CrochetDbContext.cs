7c2c76651d071a3cd4fd0797cd8625405b654350
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-03-20 18:12:40 +0200

    Added DbContext

---
+++ CrochetLibrary/CrochetDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CrochetLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrochetLibrary.Data;
+public class CrochetDbContext : DbContext
+{
+    public CrochetDbContext(DbContextOptions<CrochetDbContext> options) : base(options) { }
+
+    public DbSet<Toy> Toys { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Toy>()
+            .Property(t => t.Price)
+            .HasPrecision(10, 2);
+    }
+}
+
