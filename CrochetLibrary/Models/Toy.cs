448e14ec3c2066d99526da39e8ee78592f7c8ea6
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-03-24 20:39:25 +0200

    Added toy model

---
+++ CrochetLibrary/Models/Toy.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrochetLibrary.Models
+{
+    public class Toy
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(500)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0.01, 10000)]
+        public double Price { get; set; }
+
+        [MaxLength(50)]
+        public string Type { get; set; } = string.Empty;
+
+        [MaxLength(50)]
+        public string Yarn { get; set; } = string.Empty;
+
+        [Range(0, 50)]
+        public int Hook { get; set; }
+    }
+
+}
