350eb037302c74ee53a6d2a370d7edaaa7c2fd13
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-03-27 18:40:08 +0200

    Added toy dto

---
+++ CrochetLibrary/DataTransferObject/ToyDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace CrochetLibrary.DTOs
+{
+    public class ToyDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+        public string Colors { get; set; } = "Red, Blue, Green";
+        public int Stock { get; set; }
+    }
+}
