4fec499c68461ea3f970861cc827cf9b794389d2
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-03-27 18:06:43 +0200

    Added Itoy repository interface

---
+++ CrochetLibrary/Interfaces/IToyRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace CrochetLibrary.Repositories
+{
+    public interface IToyRepository
+    {
+        Task<IEnumerable<Toy>> GetAllAsync();
+        Task<Toy?> GetByIdAsync(int id);
+        Task<Toy> AddAsync(Toy toy);
+        Task<bool> UpdateAsync(Toy toy);
+        Task<bool> DeleteAsync(int id);
+    }
+}
