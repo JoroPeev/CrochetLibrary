69ab0a9c44c7c538c68d584191f8580732af5032
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-04-02 17:52:04 +0300

    Added intreface IOrderService

---
+++ CrochetLibrary/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,11 @@
+﻿using CrochetLibrary.Models;
+
+namespace CrochetLibrary.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<int> CreateOrderAsync(Order order);
+        Task<List<Order>> GetOrdersAsync();
+        Task<bool> UpdateOrderStatusAsync(int id, string newStatus);
+    }
+}
