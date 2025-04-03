a4c5bf4e20c2bfeedf59ee3e5a99b416d947e847
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-04-03 21:27:31 +0300

    Added iauth service

---
+++ CrochetLibrary/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using CrochetLibrary.Models.Auth;
+using Microsoft.AspNetCore.Identity;
+
+namespace CrochetLibrary.Services.Auth
+{
+    public interface IAuthService
+    {
+        Task<IdentityResult> RegisterAsync(RegisterModel model);
+        Task<bool> LoginAsync(LoginModel model);
+        Task<bool> AddAdminRoleAsync(string email);
+    }
+}
