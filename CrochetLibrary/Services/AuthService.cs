88079f6d597fd448defe31b61712c5236b8495a1
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-04-03 21:27:44 +0300

    Added auth service

---
+++ CrochetLibrary/Services/AuthService.cs
@@ -0,0 +1,46 @@
+﻿using CrochetLibrary.Models.Auth;
+using Microsoft.AspNetCore.Identity;
+
+namespace CrochetLibrary.Services.Auth
+{
+    public class AuthService : IAuthService
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(UserManager<IdentityUser> userManager,
+                           SignInManager<IdentityUser> signInManager,
+                           RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IdentityResult> RegisterAsync(RegisterModel model)
+        {
+            var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+            return await _userManager.CreateAsync(user, model.Password);
+        }
+
+        public async Task<bool> LoginAsync(LoginModel model)
+        {
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+            return result.Succeeded;
+        }
+
+        public async Task<bool> AddAdminRoleAsync(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+                return false;
+
+            if (!await _roleManager.RoleExistsAsync("Administrator"))
+                await _roleManager.CreateAsync(new IdentityRole("Administrator"));
+
+            await _userManager.AddToRoleAsync(user, "Administrator");
+            return true;
+        }
+    }
+}
