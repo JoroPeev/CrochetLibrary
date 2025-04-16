54799a21066b54f6f5cd05ebcb34bb67c16e2e2c
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-04-16 22:14:45 +0300

    Added allow angular policy

--- CrochetLibrary/Program.cs
+++ CrochetLibrary/Program.cs
@@ -1,11 +1,11 @@
-using CrochetLibrary.Data;
+﻿using CrochetLibrary.Data;
 using CrochetLibrary.Middleware;
+using CrochetLibrary.Services; // Assuming ToyService is in this namespace
 using CrochetLibrary.Services.Auth;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -19,12 +19,20 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddDefaultTokenProviders();
 
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<OrderService>();
+builder.Services.AddScoped<ToyService>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp",
+        policy => policy.WithOrigins("http://localhost:4200")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+});
 
 var app = builder.Build();
 
 app.UseMiddleware<ExceptionMiddleware>();
-
 
 if (app.Environment.IsDevelopment())
 {
@@ -33,6 +41,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAngularApp");
 
 app.UseAuthorization();
 
