3d662827aaf73e9e1f9e0413a83122f01e39d82c
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-04-22 20:08:02 +0300

    Added to program  JwtBearer

--- CrochetLibrary/Program.cs
+++ CrochetLibrary/Program.cs
@@ -2,10 +2,32 @@
 using CrochetLibrary.Middleware;
 using CrochetLibrary.Services;
 using CrochetLibrary.Services.Auth;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+var key = builder.Configuration["Jwt:Key"];
+
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+    };
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -37,14 +59,22 @@ app.UseMiddleware<ExceptionMiddleware>();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Crochet API v1");
+        c.InjectStylesheet("/swagger/custom.css");  // Optional: Add custom styles
+        c.DefaultModelsExpandDepth(-1);  // Optional: Hide models if you prefer a cleaner UI
+    });
 }
+
 
 app.UseHttpsRedirection();
 
 app.UseCors("AllowAngularApp");
 
+app.UseAuthentication();
 app.UseAuthorization();
+
 
 app.MapControllers();
 
