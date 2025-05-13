69f999b3b0370d38424aaff72e775e6837e83055
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-05-13 18:26:27 +0300

    Added customer request model

---
+++ CrochetLibrary/Models/CustomerRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace CrochetLibrary.Models
+{
+    public class CustomerRequest
+    {
+        public int Id { get; set; }
+        public string ToyName { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public string Message { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
