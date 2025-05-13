6ff28547bec5ed124f739ee17a08539cde7d4cc2
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-05-13 18:25:52 +0300

    Added requestController

---
+++ CrochetLibrary/Controllers/RequestsController.cs
@@ -0,0 +1,23 @@
+﻿using CrochetLibrary.Data;
+using CrochetLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RequestsController : ControllerBase
+{
+    private readonly CrochetDbContext _context;
+
+    public RequestsController(CrochetDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostRequest([FromBody] CustomerRequest request)
+    {
+        _context.Requests.Add(request);
+        await _context.SaveChangesAsync();
+        return Ok();
+    }
+}
