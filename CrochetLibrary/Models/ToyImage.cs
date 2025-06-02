1f454a2bb39a295855516609787e329611253965
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-06-02 21:16:32 +0300

    Added toy image model

---
+++ CrochetLibrary/Models/ToyImage.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrochetLibrary.Models
+{
+    public class ToyImage
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [MaxLength(500)]
+        public string ImageUrl { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        public string AltText { get; set; } = string.Empty;
+
+        [Range(0, 100)]
+        public int DisplayOrder { get; set; } = 0;
+
+        public bool IsPrimary { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public Guid ToyId { get; set; }
+
+        [ForeignKey("ToyId")]
+        public virtual Toy? Toy { get; set; }
+    }
+}
