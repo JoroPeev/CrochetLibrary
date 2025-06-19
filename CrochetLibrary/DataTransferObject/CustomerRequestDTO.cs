1b3a5591497187985322808c2c9b8fedc20e898c
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-06-19 19:17:41 +0300

    Added customer request dto

---
+++ CrochetLibrary/DataTransferObject/CustomerRequestDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrochetLibrary.DataTransferObject
+{
+    public class CustomerRequestDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public Guid ToyId { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        public string Message { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime DueDate { get; set; }
+
+    }
+}
