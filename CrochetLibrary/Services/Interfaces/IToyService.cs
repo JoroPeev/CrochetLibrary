10795ae905ceb3d0f8355c04226865a7c47022c7
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-06-26 18:13:58 +0300

    Removed actions from toy intrface

--- CrochetLibrary/Services/Interfaces/IToyService.cs
+++ CrochetLibrary/Services/Interfaces/IToyService.cs
@@ -5,20 +5,13 @@ namespace CrochetLibrary.Services
     public interface IToyService
     {
         Task<List<Toy>> GetToysAsync();
-        Task<List<Toy>> GetToysWithAllImagesAsync();
         Task<Toy?> GetToyByIdAsync(Guid id);
-        Task<Toy?> GetToyByIdBasicAsync(Guid id);
         Task<Toy> AddToyAsync(Toy toy);
-        Task<Toy> AddToyWithImagesAsync(Toy toy, List<string> imageUrls);
         Task<bool> AddImagesToToyAsync(Guid toyId, List<string> imageUrls);
         Task<bool> UpdateToyAsync(Guid id, Toy toy);
-        Task<bool> UpdateToyWithImagesAsync(Guid id, Toy toy, List<string>? newImageUrls = null);
         Task<bool> DeleteToyAsync(Guid id);
-        Task<bool> SetPrimaryImageAsync(Guid toyId, Guid imageId);
         Task<bool> DeleteImageAsync(Guid imageId);
-        Task<bool> UpdateImageOrderAsync(Guid toyId, List<Guid> imageIdsInOrder);
         Task<List<ToyImage>> GetToyImagesAsync(Guid toyId);
-        Task<List<Toy>> SearchToysAsync(string searchTerm);
-        Task<object> GetToyStatisticsAsync();
+        Task<bool> UpdateImageAsync(Guid imageId, string newImageUrl, int? newDisplayOrder = null);
     }
 }
