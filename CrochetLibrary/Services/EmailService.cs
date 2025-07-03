e769109052ed870a07321a11dc7c65dc639bc94e
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-07-03 18:08:17 +0300

    Added email service

---
+++ CrochetLibrary/Services/EmailService.cs
@@ -0,0 +1,28 @@
+﻿using CrochetLibrary.Services.Interfaces;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+public class EmailService : IEmailService
+{
+    private readonly IConfiguration _config;
+
+    public EmailService(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public void SendEmail(string to, string subject, string body)
+    {
+        var email = new MimeMessage();
+        email.From.Add(MailboxAddress.Parse(_config["EmailSettings:From"]));
+        email.To.Add(MailboxAddress.Parse(to));
+        email.Subject = subject;
+        email.Body = new TextPart("plain") { Text = body };
+
+        using var smtp = new SmtpClient();
+        smtp.Connect(_config["EmailSettings:SmtpServer"], 587, MailKit.Security.SecureSocketOptions.StartTls);
+        smtp.Authenticate(_config["EmailSettings:From"], _config["EmailSettings:Password"]);
+        smtp.Send(email);
+        smtp.Disconnect(true);
+    }
+}
