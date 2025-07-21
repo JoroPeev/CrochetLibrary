b391b46cd966ef57480a3f94958a02d192431551
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-07-21 18:33:24 +0300

    Added Review dto

---
+++ CrochetLibrary/DataTransferObject/ReviewDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrochetLibrary.DataTransferObject
+{
+    public class ReviewDto
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Your Email")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Name cannot exceed 50 characters.")]
+        [RegularExpression(@"^[a-zA-Z\s\-']+$", ErrorMessage = "Name can only contain letters, spaces, hyphens, or apostrophes.")]
+        [Display(Name = "Your Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "Comment cannot exceed 500 characters.")]
+        [Display(Name = "Your Review")]
+        public string Comment { get; set; }
+
+        public DateTime ReviewDate { get; set; } = DateTime.UtcNow;
+    }
+}
