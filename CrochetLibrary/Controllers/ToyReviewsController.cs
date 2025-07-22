d0d7598f5fbda349740c80d8891e8a8c25947a8a
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-07-22 16:32:14 +0300

    Separated toy reviews from toy actions

---
+++ CrochetLibrary/Controllers/ToyReviewsController.cs
@@ -0,0 +1,57 @@
+﻿using CrochetLibrary.DataTransferObject;
+using CrochetLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrochetLibrary.Controllers
+{
+    [Route("api/toys/{id:guid}/reviews")]
+    [ApiController]
+    public class ToyReviewsController : ControllerBase
+    {
+        private readonly IToyService _toyService;
+
+        public ToyReviewsController(IToyService toyService)
+        {
+            _toyService = toyService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Review>>> GetReviews([FromRoute] Guid id)
+        {
+            var reviews = await _toyService.GetToyReviewsAsync(id);
+            return Ok(reviews ?? new List<Review>());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddReviewToToy([FromRoute] Guid id, [FromBody] ReviewDto reviewDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var review = new Review
+            {
+                EmailAddress = reviewDto.EmailAddress,
+                Name = reviewDto.Name,
+                Comment = reviewDto.Comment,
+                ReviewDate = reviewDto.ReviewDate,
+                ToyId = id
+            };
+
+            var result = await _toyService.AddReviewToToyAsync(id, review);
+            if (!result)
+                return NotFound();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{reviewId:guid}")]
+        public async Task<IActionResult> DeleteReview([FromRoute] Guid id, [FromRoute] Guid reviewId)
+        {
+            var deleted = await _toyService.DeleteReviewAsync(reviewId);
+            if (!deleted)
+                return NotFound();
+
+            return NoContent();
+        }
+    }
+}
